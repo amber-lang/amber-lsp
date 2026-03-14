361c46c85385083a45cae43d9598b9b940ceb67f
Hubert Jabłoński <hubik080@gmail.com>
2026-03-14 19:14:05 +0100

    Added bigger sample

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::sync::Arc;
+use std::time::Duration;
 
 use amber_analysis::files::DEFAULT_VERSION;
 use amber_analysis::{
@@ -164,7 +165,8 @@ fn bench_parse(c: &mut Criterion) {
 fn bench_analysis(c: &mut Criterion) {
     let mut group = c.benchmark_group("analysis");
 
-    group.sample_size(10);
+    group.sample_size(30);
+    group.measurement_time(Duration::from_secs(45));
 
     for v in versions() {
         let source = load_stdlib_source(v.resource_dir);
